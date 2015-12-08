80cbe5bf1b4da9523112c4d25695ef3553397c09
Abdallah Ben Othman <ben_othman@live.fr>
2015-12-08 21:08:01 +0100

    Correction bug equipment_in_dungeon

--- Project/MortalSpace/scripts/HUD.gd
+++ Project/MortalSpace/scripts/HUD.gd
@@ -7,8 +7,8 @@ extends Control
 
 #An other solution : place this function in _process(delta), but I think this solution cost a lot for nothing
 func refresh_ammo():
-	var ammo_in_weapon = get_node("/root/equipement_in_dungeon").get_ammo_in_weapon()
-	var ammo_left      = get_node("/root/equipement_in_dungeon").get_ammo_left()
+	var ammo_in_weapon = get_node("/root/equipment_in_dungeon").get_ammo_in_weapon()
+	var ammo_left      = get_node("/root/equipment_in_dungeon").get_ammo_left()
 	var text_to_print  = str(ammo_in_weapon) + "/" + str(ammo_left)
 	get_node("ammo_number").set_text(text_to_print)
 
